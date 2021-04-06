c316f5d7e53386b8329eac7245d4f1852745b183
danielfernandoyepezvelez <danielfernandoyepezvelez>
2021-04-06 17:54:54 -0500

    Agregando La Parte De Las Rutas

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components/src/pages/alert_page.dart';
+import 'package:flutter_components/src/pages/avatar_page.dart';
 
 // import 'package:flutter_components/src/pages/home_temp.dart';
 import 'package:flutter_components/src/pages/home_page.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // return MaterialApp(title: 'Material App', home: HomePageTemp());
-    return MaterialApp(title: 'Material App', home: HomePage());
+    return MaterialApp(
+      title: 'Material App',
+      // home: HomePage();
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (BuildContext context) => HomePage(),
+        'alert': (BuildContext context) => AlertPage(),
+        'avatar': (BuildContext context) => AvatarPage(),
+      },
+    );
   }
 }
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_components/src/providers/menu_provider.dart';
+import 'package:flutter_components/src/utils/icono_string_util.dart';
+
+import 'package:flutter_components/src/pages/alert_page.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -25,21 +28,28 @@ class HomePage extends StatelessWidget {
         // print(snapshot.data);
 
         return ListView(
-          children: _listaItems(snapshot.data),
+          children: _listaItems(snapshot.data, context),
         );
       },
     );
   }
 
-  List<Widget> _listaItems(List<dynamic> data) {
+  List<Widget> _listaItems(List<dynamic> data, BuildContext context) {
     final List<Widget> itemsFinales = [];
 
     data.forEach((item) {
       final widgetTemp = ListTile(
-        leading: Icon(Icons.account_circle, color: Colors.blue),
+        leading: getIcon(item['icon']),
         title: Text(item['texto']),
         trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
-        onTap: () {},
+        onTap: () {
+          /* Forma De Navegación Sencilla, Para Ir Y Volver */
+          /* final route = MaterialPageRoute(builder: (context) => AlertPage());
+          Navigator.push(context, route); */
+
+          /* Ruta Más Sencilla */
+          Navigator.pushNamed(context, item['ruta']);
+        },
       );
 
       itemsFinales..add(widgetTemp)..add(Divider());
