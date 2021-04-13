93f4511a4ade13777284c83772b2f709ddf33711
danielfernandoyepezvelez <danielfernandoyepezvelez>
2021-04-13 15:10:03 -0500

    Agregando Animaciones Y Avatars

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -3,6 +3,35 @@ import 'package:flutter/material.dart';
 class AvatarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Avatar Page')));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Avatar Page'),
+        actions: [
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://www.geekmi.news/__export/1605198940916/sites/debate/img/2020/11/12/stan_leex_su_legado_a_dos_axos_de_su_muerte.jpg_242310155.jpg'),
+              radius: 30.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage(
+              'https://www.geekmi.news/__export/1605198940916/sites/debate/img/2020/11/12/stan_leex_su_legado_a_dos_axos_de_su_muerte.jpg_242310155.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
+      ),
+    );
   }
 }
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components/src/pages/animated_container.dart';
 
 import 'package:flutter_components/src/pages/home_page.dart';
 import 'package:flutter_components/src/pages/alert_page.dart';
@@ -12,5 +13,6 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'alert': (BuildContext context) => AlertPage(),
     'avatar': (BuildContext context) => AvatarPage(),
     'card': (BuildContext context) => CardPage(),
+    'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
   };
 }
